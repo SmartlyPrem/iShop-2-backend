5b8ed5e42de073904a0eec39bafd8bd1df5d6d32
Prem Jatol <140836410+SmartlyPrem@users.noreply.github.com>
2024-08-09 20:58:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,10 +10,9 @@ const OrderRouter = require('./routers/Order');
 // const adminAuth = require('./middleware/adminAuth');
 const TransitionRouter = require('./routers/Transition');
 const AdminRouter = require('./routers/Admin');
-const port = process.env.PORT || 5000;
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
